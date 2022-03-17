c1e1fda892ed677565ccdf130b9929d31b2dd029
Вигерин Эдуард <vigerin@tochkak.ru>
2022-03-17 19:57:36 +0300

    added django app

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 import requests
+
 import lxml
+import django
+import djangorestframework
+
 
 
 def get_page():
@@ -11,11 +16,22 @@ def get_page():
 if __name__ == '__main__':
     html = get_page()
     soup = BeautifulSoup(html, 'lxml')
+    news = []
 
     refs = soup.find_all('a', href=True)
-    news = {}
+    # td = soup.find_all('td', class_='title')
+    count = 1
     for ref in refs:
         if ref.attrs.get('class'):
             if 'titlelink' in ref.attrs.get('class'):
-                print(ref.text, '\n', ref.attrs.get('href'))
-    print()
+                # print(ref.text, '\n', ref.attrs.get('href'))
+                new_one = {
+                    "id": count,
+                    "title": ref.text,
+                    "url": ref.attrs.get('href'),
+                    'created': datetime.now()
+                }
+                news.append(new_one)
+                count += 1
+
+    print(news)
